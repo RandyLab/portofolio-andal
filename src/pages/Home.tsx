4e5772938ec26cafec4697a56f31cabe066ea055
RandyLab <nuggetnyafiesta45@gmail.com>
2026-01-30 11:46:00 +0700

    update home hancur

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
-    <div className="text-center py-10">
-      <h1 className="text-3xl font-bold">Welcome to My Portfolio</h1>
-      <p className="mt-4 text-gray-600">This is the Home page.</p>
-    </div>
+    <section className="text-center flex mt-8">
+      <div className="h-screen bg-black flex flex-col justify-center items-center">
+        <h1 className="text-3xl font-bold">Welcome to My Portfolio</h1>
+        <p className="mt-4 text-gray-600">This is the Home page.</p>
+      </div>
+    </section>
   );
 }
